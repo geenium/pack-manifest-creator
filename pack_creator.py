53b07aacdf206cb79ba59d699db0c7dec20c0f4e
geenium <geenium@users.noreply.github.com>
2017-08-30 18:59:23 +0100

    Added support for skin packs

--- pack_creator.py
+++ pack_creator.py
@@ -18,8 +18,8 @@ def create_manifest(name, data_type, description):
     file = "manifest.json"
     with open(file, "w") as f:
         # Contents of the manifest for behaviour/resource packs
-        contents = '{{\n\t"format_version": 1,\n\t"header": {{\
-\n\t\t"description": "{}",\n\t\t"name": "{}",\n\t\t"uuid": "{}",\
+        contents = '{{\n\t"format_version": 1,\n\t"header": {{{}\
+\n\t\t"name": "{}",\n\t\t"uuid": "{}",\
 \n\t\t"version": [ 0, 0, 1 ]\n\t}},\n\t"modules": [\n\t\t{{\
 \n\t\t\t"type": "{}",\n\t\t\t"uuid": "{}",\n\t\t\t"version": [ 0, 0, 1 ]\
 \n\t\t}}\n\t]\n}}'
@@ -43,33 +43,44 @@ def create_manifest(name, data_type, description):
     print("New {} pack manifest successfully created!\n".format(data_type))
 
 
-# Gathers information for new resourcepacks and behaviour packs
+# Creates the zip folder for new resourcepacks and behaviour packs
 def new_manifest():
     # Asks whether a resourcepack or behaviour pack should be made
-    pack = input("Do you want a resource pack or behaviour pack? (r/b)\n\
->> ")
+    pack = input("Do you want a resource pack, behaviour pack, or skin pack? \
+(r/b/s)\n>> ")
 
     # Only accepts 'r' or 'b' as input
-    while pack not in ["r", "b"]:
-        pack = input("Please enter 'r' for resource pack or 'b' for \
-behaviour pack!\n>> ")
+    while pack not in ["r", "b", "s"]:
+        pack = input("Please enter 'r' for resource pack, 'b' for behaviour \
+pack, or 's' for skin pack!\n>> ").lower()
 
     # Sets certain values for resourcepacks
     if pack == "r":
         input_type = "resource"
         manifest_type = "resources"
     # Sets certain values for behaviour packs
-    else:
+    elif pack == "b":
         input_type = "behavior"
         manifest_type = "data"
+    # Sets certain values for skin packs
+    else:
+        input_type = "skin"
+        manifest_type = "skin_pack"
 
     # Gets the name and description for the behaviour/resource pack
     name = input("What is the name of the {} pack?\n>> ".format(input_type))
 
-    description = input("What will the description for the {} pack be?\
-\n>> ".format(input_type))
+    # Skin packs don't need a description
+    description = ""
+    if pack != "s":
+        description = '\n\t\t"description": "{}",'.format(input("What will \
+the description for the {} pack be?\n>> ".format(input_type)))
 
     create_manifest(name, manifest_type, description)
+
+# # # # # # # # # # # # # # # # # #
+# This is where the script begins #
+# # # # # # # # # # # # # # # # # #
 
 # Asks if a new pack should be created, if not the program stops
 while yes_no(input("Do you want to create a new pack? (y/n)\n>> ").lower()):
